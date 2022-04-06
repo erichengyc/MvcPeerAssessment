2ddb99f84442ac5b47e2bfc944f28e9534944a2e
erichengyc <68725887+erichengyc@users.noreply.github.com>
2022-04-06 22:22:22 +0930

    Add project files.

---
+++ MvcPeerAssessment/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcPeerAssessment.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home/index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcPeerAssessment/Controllers/ProjectsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcPeerAssessment.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MvcPeerAssessment.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        [HttpGet]
+        [Route("api/projects")]
+        public List<Project> Get()
+        {
+            PeerAssessmentDbContext db = new PeerAssessmentDbContext();
+            List<Project> projects = db.Projects.ToList();
+            return projects;
+        }
+    }
+}
